aa523c2f9ab1f8aa7902bbee62359a6c07263c40
neelmani07 <manineel25@gmail.com>
2021-08-24 14:42:22 +0530

    Added custom validaters for components types

--- djAssignment/models.py
+++ djAssignment/models.py
@@ -1,17 +1,29 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
 from django.db.models.fields import NullBooleanField
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
+def validate_type_of_icons(value):
+        list_type = ['icon','image','heading','body','link']
+        if value not in list_type:
+            raise ValidationError(
+                _('not one of the icon types. must be from %s'%(list_type).__str__())
+            )
 class Compo(models.Model):
 
-    type_of_compo = models.CharField(max_length=50)
+    type_of_compo = models.CharField(max_length=50,validators=[validate_type_of_icons])
     val = models.CharField(max_length=50)
     style = models.CharField(max_length=50)
     nodes = models.CharField(max_length=50, default="systems")
     pageAssigned = models.ForeignKey('Page',on_delete=CASCADE, blank=True, null=True)
-
+    
     def save(self, *args, **kwargs):
         self.nodes = "system"
+        try:
+            Compo.full_clean(self)
+        except ValidationError as e:
+            pass
         super(Compo, self).save(*args, **kwargs)
 
     def __str__(self):
